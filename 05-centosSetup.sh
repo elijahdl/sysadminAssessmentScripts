170613429ab014c56c13a772dd90673275604094
Elijah Love <eli.dlove@gmail.com>
2018-03-07 13:38:50 -0500

    Added some logic

--- 05-centosSetup.sh
+++ 05-centosSetup.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-# Join to domain
+# Get some parameters
+echo "Enter domain:"
+read domainName
+echo "Enter domain admin:"
+read domainUser
+echo "Enter name of domain admin group"
+read adminGroup
 
-# Add users to admin group
+# Join to domain
+sudo yum install -y realmd samba samba-common oddjob oddjob-mkhomedir sssd
+sudo realm join --user=$domainUser@$domainName $domainName 
+
+# Adjust sudoers file for domain admin group 
+
+if [ -z "$1" ] ; then
+  export EDITOR=$0 && sudo -E visudo
+else
+  echo "%$adminGroup@$domainName      ALL=(ALL)       ALL" >> $1
+fi
 
 # Setup sshd
 
